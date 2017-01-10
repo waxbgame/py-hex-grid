33c1a7ad3638fa0e09d9778733687f5a34a30505
waxbgame <waxbgame@gmail.com>
2017-01-09 19:50:55 -0800

    Update brute_force_hexagon.py
    using for-loop and map function to refactor to more generic drawing function

--- src/brute_force_hexagon.py
+++ src/brute_force_hexagon.py
@@ -2,24 +2,29 @@ import simplegui
 from math import cos, sin, radians
 
 def draw(canvas):
+    REGULAR_HEXAGON_ANGLE = 60
     size = 100
     pos = (300,300)
     FLAT_TOPPED = 0
     POINTY_TOPPED = 30
-    
+
     arc_pos = POINTY_TOPPED
-    canvas.draw_polygon(
-        [(cos(radians(arc_pos))*size+pos[0],sin(radians(arc_pos))*size+pos[1]),
-         (cos(radians(arc_pos + 60))*size+pos[0],sin(radians(arc_pos + 60))*size+pos[1]),
-         (cos(radians(arc_pos + 2*60))*size+pos[0],sin(radians(arc_pos + 2*60))*size+pos[1]),
-         (cos(radians(arc_pos + 3*60))*size+pos[0],sin(radians(arc_pos + 3*60))*size+pos[1]),
-         (cos(radians(arc_pos + 4*60))*size+pos[0],sin(radians(arc_pos + 4*60))*size+pos[1]),
-         (cos(radians(arc_pos + 5*60))*size+pos[0],sin(radians(arc_pos + 5*60))*size+pos[1])],
-         1,
-         'Red',
-         'Blue'
+    num_points = 6
+    regular_angles = 360 / num_points
+    points = []
+    for arc_degree in range(360/regular_angles):
+        points.append(
+            (cos(radians(arc_pos + arc_degree*regular_angles)),
+             sin(radians(arc_pos + arc_degree*regular_angles))
+            )
         )
-#(cos(330)*size+pos[0],sin(330)*size+pos[1])], 
+    points = map(lambda point: (point[0]*size+pos[0],point[1]*size+pos[1]), points)
+    canvas.draw_polygon(
+        points,
+        1,
+        'Red',
+        'Blue'
+    )
 frame = simplegui.create_frame("Home", 500, 500)
 frame.set_draw_handler(draw)
 frame.start()
