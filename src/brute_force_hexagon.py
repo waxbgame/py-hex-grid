4a1182dda3744b187c8df73d8248b75f7238e420
waxbgame <waxbgame@gmail.com>
2017-01-10 18:14:23 -0800

    Update brute_force_hexagon.py
    refactored to a builder, moved the coord function out of draw

--- src/brute_force_hexagon.py
+++ src/brute_force_hexagon.py
@@ -1,15 +1,47 @@
 import simplegui
 from math import cos, sin, radians
 
-def draw(canvas):
-    REGULAR_HEXAGON_ANGLE = 60
-    size = 100
-    pos = (300,300)
-    FLAT_TOPPED = 0
-    POINTY_TOPPED = 30
+class RegularShapeMaker:
+    
+    class RegularShape:
+        
+        def __init__(self, builder):
+            self.point_count = builder.point_count
+            self.pixel_radius = builder.pixel_radius
+            self.pos = builder.pos
+            self.arc_pos_initial = builder.arc_pos_initial
+        
+    def with_num_points(self, point_count):
+        self.point_count = point_count
+        return self
+    
+    def with_pixel_radius(self, pixel_radius):
+        self.pixel_radius = pixel_radius
+        return self
+    
+    def with_initial_position_xy(self, pos):
+        self.pos = pos
+        return self
+    
+    def with_initial_arc_pos_degrees(self, degrees):
+        self.arc_pos_initial = degrees
+        return self
+    
+    def build(self):
+        return self.RegularShape(self)
 
-    arc_pos = POINTY_TOPPED
-    num_points = 6
+hexagon = RegularShapeMaker() \
+    .with_num_points(6) \
+    .with_pixel_radius(100) \
+    .with_initial_position_xy((200,200)) \
+    .with_initial_arc_pos_degrees(0) \
+    .build()
+
+def coords_for_shape(shape):
+    pos = shape.pos
+    size = shape.pixel_radius
+    arc_pos = shape.arc_pos_initial
+    num_points = shape.point_count
     regular_angles = 360 / num_points
     points = []
     for arc_degree in range(360/regular_angles):
@@ -19,12 +51,18 @@ def draw(canvas):
             )
         )
     points = map(lambda point: (point[0]*size+pos[0],point[1]*size+pos[1]), points)
-    canvas.draw_polygon(
+    return points
+
+points = coords_for_shape(hexagon)
+
+def draw(canvas):
+        canvas.draw_polygon(
         points,
-        1,
-        'Red',
+        5,
+        'Orange',
         'Blue'
     )
+
 frame = simplegui.create_frame("Home", 500, 500)
 frame.set_draw_handler(draw)
 frame.start()
